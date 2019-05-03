db5a69c80b47b1a3110daa8dd7e6599b66788015
Bas van Kuijck <bas@e-sites.nl>
2019-05-03 11:59:45 +0200

    accio

---
+++ Package.swift
@@ -0,0 +1,51 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "ProntoSDK",
+    products: [
+        .library(name: "ProntoSDK", targets: ["ProntoSDK"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", .upToNextMajor(from: "3.2.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/Igor-Palaguta/Cache", .revision("560f00a9a9549db161ca70d96eed74fc580b03e3")), // See https://github.com/hyperoslo/Cache/issues/238
+        .package(url: "https://github.com/google/promises", .upToNextMajor(from: "1.2.8")),
+        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "5.0.1")),
+
+        // Testing
+        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "8.0.1")),
+
+        // E-sites
+//        .package(url: "https://github.com/e-sites/Cobalt", .branch("master")),
+        .package(url: "../#ios-suite/Erbium", .branch("master")),
+//        .package(url: "https://github.com/e-sites/Einsteinium", .branch("master")),
+//        .package(url: "https://github.com/e-sites/Lithium", .branch("master")),
+//        .package(url: "https://github.com/e-sites/Mockingjay", .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "ProntoSDK",
+            dependencies: [
+                "SwiftyJSON",
+                "KeychainAccess",
+                "CryptoSwift",
+                "Cache",
+                "Promises",
+                "RxSwift",
+                "Erbium"
+            ],
+            path: "ProntoSDK"
+        ),
+        .testTarget(
+            name: "ProntoSDKTests",
+            dependencies: [
+                "Nimble",
+                "Mockingjay",
+                "Lithium"
+            ],
+            path: "ProntoSDKTests"
+        )
+    ]
+)
